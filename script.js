52566e8be6f2068ee1b23fd218487c5cc331df1c
maxguzmanh <max.guzmanh@duocuc.cl>
2024-04-24 15:35:44 -0400

    imagen de fondo

--- script.js
+++ script.js
@@ -39,8 +39,33 @@ $(document).ready(function() {
     });
   
     // Manejar el evento de clic en el botón "Crear Carta"
-    $('#crearCarta').on('click', function() {
-      // Aquí iría la lógica para crear la carta de presentación
-    });
+  $('#crearCarta').on('click', function() {
+    // Recolectar los datos del formulario
+    var rut = $('#rut').val();
+    var apellidoPaterno = $('#apellidoPaterno').val();
+    var apellidoMaterno = $('#apellidoMaterno').val();
+    var nombre = $('#nombre').val();
+    var fechaNacimiento = $('#fechaNacimiento').val();
+    var edad = $('#edad').val();
+    var genero = $('#genero').val();
+    var email = $('#email').val();
+    var celular = $('#celular').val();
+    var profesion = $('#profesion').val();
+    var motivacion = $('#motivacion').val();
+
+    // Construir la carta de presentación
+    var carta = "Estimado/a reclutador/a,\n\n";
+    carta += "Mi nombre es " + nombre + " " + apellidoPaterno + " " + apellidoMaterno + " y mi rut es " + rut + ". ";
+    carta += "Nací el " + fechaNacimiento + ", tengo " + edad + " años y me identifico como " + genero + ". ";
+    carta += "Puede contactarme a través de mi correo electrónico " + email + " o mi número de celular " + celular + ".\n\n";
+    carta += "Soy " + profesion + " de profesión y estoy muy interesado/a en el trabajo de apoyo ambiental en Chiloé. ";
+    carta += "Me motiva el desafío de contribuir al cuidado del medio ambiente y estoy convencido/a de que mi experiencia y motivación me hacen un/a candidato/a ideal para esta posición.\n\n";
+    carta += "A continuación, adjunto mi motivación para postular al trabajo:\n" + motivacion + "\n\n";
+    carta += "Quedo a disposición para cualquier consulta o entrevista que deseen realizar.\n\n";
+    carta += "Atentamente,\n" + nombre + " " + apellidoPaterno + " " + apellidoMaterno;
+
+    // Mostrar la carta en el área de texto
+    $('#carta').val(carta);
   });
+});
   
